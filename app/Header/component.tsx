db95c030015ddbb9c063559b6de0545330d63c52
Sina Klughardt <sina.klughardt@gmail.com>
2026-03-14 17:06:25 -0700

    landing page layout and font

--- app/Header/component.tsx
+++ app/Header/component.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Link from 'next/link'
 import logo from "../Media/Sunset cliffs.png"
 import Image from 'next/image'
+import { Fjalla_One } from "next/font/google"
+
+const fjalla = Fjalla_One({
+  weight: "400",
+  subsets: ["latin"],
+})
 
 
 
 export function Header() {
   return (
     <>
-    <div className="flex justify-between mx-auto  md:pr-4 md:pl-4">
+    <div className={`${fjalla.className} antialiased flex justify-between mx-auto  md:pr-4 md:pl-4`}>
       <div className="flex items-center space-x-5" id="navbar-default">
         <Link href="/">
         <Image src={logo} alt="logo" width={200} height={300} className="rounded h-[80px] w-[80px]"/>
@@ -16,16 +22,16 @@ export function Header() {
           </div>
       <ul className="flex items-center space-x-5 ml-auto mask-t-from-50% ">
         <li>
-          <Link href="/" >Home</Link>
+          <Link href="/" >HOME</Link>
         </li>
         <li>
-          <Link href="/members">Projects</Link>
+          <Link href="/members">PROJECTS</Link>
         </li>
         <li>
-          <Link href="/aboutus">About us</Link>
+          <Link href="/aboutus">ABOUT US</Link>
         </li>
         <li>
-          <Link href="/contact">Contact</Link>
+          <Link href="/contact">CONTACT</Link>
         </li>
       </ul>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,45 @@
 import Image from "next/image";
 import logo from "./Media/puter.png"
+import { Fjalla_One } from "next/font/google"
 
+const fjalla = Fjalla_One({
+  weight: "400",
+  subsets: ["latin"],
+})
 
 export default function Home() {
+
+
   return (
 <div>
 
-  <div className="md:flex h-auto justify-center  pt-5">
-    <div className="md:w-1/2 mx-5 justify-end flex ">
-    <div className="mt-20">
-      <h1 className="text-4xl">we are coding code</h1>
-      <p>heres a short description on why you should hire us</p>
+  <div className="md:flex h-auto justify-center py-30 bg-gray-800">
+    <div className="md:w-1/2 max-w-[600px] mx-5 justify-end flex items-center">
+    <div>
+    <div className={`${fjalla.className} antialiased`}>
+      <h1 className="text-6xl">WE ARE CODING CODE</h1>
+      </div>
+      <p className="text-xl">heres a short description on why you should hire us</p>
+      <div className={`${fjalla.className} antialiased flex justify-start`}>
+        <button className="mt-5 mr-5 box-border bg-gray-200 text-gray-900 hover:bg-gray-300 rounded-full border rounded-base shadow-xs px-4 py-2.5 border-rounded">GET STARTED</button>
+        <button className="mt-5 box-border rounded-full hover:bg-gray-900 border rounded-base shadow-xs px-4 py-2.5  border-rounded">ABOUT US</button>
       </div>
       </div>
-    <div className="md:w-1/2 mx-5 justify-start flex">
+      </div>
+    <div className="md:w-1/2 pl-20 justify-start flex max-w-[600px]">
     <Image src={logo} alt="sunset" width={800} height={200} className="w-[300px] h-[300px]"/>
     </div>
 
   </div>
+  <div className="text-center max-w-[600px] mx-auto my-30">
+    <h1 className={`${fjalla.className} antialiased mt-20 text-5xl`}>DON'T WORRY BE HAPPY</h1>
+<div className="">never a single need to worry again because we will save your
+  life and the lives of your grandchildren and neighbors.
+  I hope you are readysjkdaljskjaskl</div>
+  <button className={`${fjalla.className} antialiased mt-5 box-border rounded-full border rounded-base shadow-xs px-4 py-2.5 border-rounded`}>PRESS ME</button>
+</div>
+<div className="bg-gray-800 py-30">yayaya
+</div>
 </div>
   );
 }
