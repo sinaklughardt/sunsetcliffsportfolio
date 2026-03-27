1c6b12df2be5d6c1ef917d8fb5557ee78e0c098a
Sina Klughardt <sina.klughardt@gmail.com>
2026-03-27 16:09:05 -0700

    more button

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -17,12 +17,12 @@ export default function Home() {
     <OurProjects />
     </div>
     <div className={`${fjalla.className} h-80 place-content-center`}>
-      <div className="lg:w-300 mx-4 lg:mx-auto justify-items-center">
+      <div className="lg:w-300 mx-4 lg:mx-auto justify-items-center px-4">
       <div className="md:flex justify-items-center ">
       <div className="md:w-3/4 text-5xl text-center md:text-left">READY TO GET THE SITE YOU ALWAYS WANTED?</div>
       <div className="md:w-1/4">
         <Link href="/contact">
-      <button className="mt-5 box-border bg-gray-200 text-gray-900 hover:bg-[#00b2b8] rounded-full border rounded-base shadow-xs px-5 py-2.5 border-rounded">GET STARTED</button>
+      <button className="box-border bg-gray-200 text-gray-900 hover:bg-[#00b2b8] rounded-full border rounded-base shadow-xs px-5 py-2.5 border-rounded">GET STARTED</button>
       </Link></div>
       </div>
 </div>
