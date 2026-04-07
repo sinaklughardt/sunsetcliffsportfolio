8ae6c55966b478c5e2ff5f8fb2c8683729661416
kypicl <kyclarke@gmail.com>
2026-04-07 16:23:38 -0700

    updated footer to web design

--- app/Footer/component.tsx
+++ app/Footer/component.tsx
@@ -2,7 +2,7 @@ export function Footer() {
     return (
         <>
         <div className="text-center border-t border-white text-white/70 pt-5 pb-3">
-            <p>Sunset Cliffs Web Solutions</p>
+            <p>Sunset Cliffs Web Design</p>
             <p>© Copyright 2026</p>
         </div>
         </>
